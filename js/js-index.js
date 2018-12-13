e4fa69b558eb0eddfcce548c0a1218e9001d5d0e
Fletcher <aldersonf@hotmail.co.uk>
2018-12-13 02:06:33 +0000

    Added Color Picker with sliders: RED BLUE GREEN
    ALPHA

--- js/js-index.js
+++ js/js-index.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+
+    // TURNING LIGHTS ON
     $("#ll-on").click(function () {
         console.log("Light State: ON");
         var xmlhttp = new XMLHttpRequest();
@@ -6,6 +8,7 @@ $(document).ready(function () {
         xmlhttp.send('{"on": true}');
     });
 
+    // TURNING LIGHTS OFF
     $("#ll-off").click(function () {
         console.log("Light State: OFF");
         var xmlhttp = new XMLHttpRequest();
@@ -13,4 +16,18 @@ $(document).ready(function () {
         xmlhttp.send('{"on": false}');
 
     });
+
+
+    setRgba();
+
 });
+
+function setRgba () {
+    var red = document.querySelector('.rgba-picker .red-slider input').value;
+    var green = document.querySelector('.rgba-picker .green-slider input').value;
+    var blue = document.querySelector('.rgba-picker .blue-slider input').value;
+    var alpha = document.querySelector('.rgba-picker .silver-slider input').value / 100;
+    var color = "rgba(" + red + "," + green + "," + blue + "," + alpha + ")";
+    document.querySelector(".rgba-picker .color-preview .color").style.backgroundColor = color;
+    console.log(color);
+}
